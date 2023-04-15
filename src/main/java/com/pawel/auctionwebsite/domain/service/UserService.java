7dabb91ef3e8b12a192f76f8bd29def916de4527
jakub.wszola <jakub.wszola@asseco.pl>
2023-04-15 09:46:16 +0200

    fixed mapstruct error

--- src/main/java/com/pawel/auctionwebsite/domain/service/UserService.java
+++ src/main/java/com/pawel/auctionwebsite/domain/service/UserService.java
@@ -18,11 +18,11 @@ public class UserService {
         return userRepository.save(new User(UUID.randomUUID().toString(),nickname, password, address, type, status));
     }
 
-    public User updateUser(String uuid, String nickname, String password, Address address) {
-        User byUuid = userRepository.findByUuid(uuid);
-
-        //sprawdz czy nickname jest nullem, jezeli nie jest nullem to uzyj go w konstruktorze do toSave, jezeli jest nullem uzyj
-        //zmiennej z byUuid- tak samo dla pozostalych(nickname, password, address)
-        User toSave = new User()
-    }
+//    public User updateUser(String uuid, String nickname, String password, Address address) {
+//        User byUuid = userRepository.findByUuid(uuid);
+//
+//        //sprawdz czy nickname jest nullem, jezeli nie jest nullem to uzyj go w konstruktorze do toSave, jezeli jest nullem uzyj
+//        //zmiennej z byUuid- tak samo dla pozostalych(nickname, password, address)
+//        User toSave = new User()
+//    }
 }
--- src/main/java/com/pawel/auctionwebsite/infrastructure/entity/UserDao.java
+++ src/main/java/com/pawel/auctionwebsite/infrastructure/entity/UserDao.java
@@ -1,28 +1,30 @@
 package com.pawel.auctionwebsite.infrastructure.entity;
+
+import com.pawel.auctionwebsite.domain.model.user.Address;
 import com.pawel.auctionwebsite.domain.model.user.Status;
 import com.pawel.auctionwebsite.domain.model.user.Type;
-import lombok.AccessLevel;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserDao {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(AccessLevel.NONE)
     Long id;
     String uuid;
     String nickname;
     String password;
-    String street;
-    String city;
-    String voivodeship;
-    String postcode;
-    String houseNumber;
+    @Embedded
+    Address address;
     Type type;
     Status status;
     LocalDateTime createdDate;
